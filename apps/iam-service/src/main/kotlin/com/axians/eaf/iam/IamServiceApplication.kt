dd7e22d3b409e77f9f12e098247d8ce86b9e2494
Walloschke, Michael <michael.walloschke@googlemail.com>
2025-06-19 13:51:09 +0200

    feat: Update Spring Boot application configuration and JPA settings for improved database management

--- apps/iam-service/src/main/kotlin/com/axians/eaf/iam/IamServiceApplication.kt
+++ apps/iam-service/src/main/kotlin/com/axians/eaf/iam/IamServiceApplication.kt
@@ -4,12 +4,11 @@ package com.axians.eaf.iam
 
 import com.axians.eaf.iam.infrastructure.config.SystemInitializationProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication
 @ComponentScan(basePackages = ["com.axians.eaf.iam"])
 @EnableConfigurationProperties(SystemInitializationProperties::class)
 class IamServiceApplication
